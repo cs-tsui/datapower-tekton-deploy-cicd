8160562cff19c8d96377724d4a89b1da6cbc0abc
Chun Sing Tsui <c.s.tsui831@gmail.com>
2020-09-02 16:26:46 -0400

    updated test logic

--- dp/soap-wsp/smoke-test.sh
+++ dp/soap-wsp/smoke-test.sh
@@ -6,30 +6,24 @@
 
 export SOUP_ROUTE_HTTPS=$(oc get route $RELEASE_NAME-calc-https -ojsonpath='{.spec.host}' -n $TARGET_NAMESPACE)
 
-ERR_CODE=1
+curl -k -X POST \
+https://$SOUP_ROUTE_HTTPS/calculator.asmx \
+-H 'Content-Type: text/xml' \
+-H 'SOAPAction: http://tempuri.org/Add' \
+-d '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">
+<soapenv:Header/>
+<soapenv:Body>
+    <tem:Add>
+        <tem:intA>123</tem:intA>
+        <tem:intB>100</tem:intB>
+    </tem:Add>
+</soapenv:Body>
+</soapenv:Envelope>'
 
-while [ "$ERR_CODE" -ne 0 ]
-do
-    curl -k -X POST \
-    https://$SOUP_ROUTE_HTTPS/calculator.asmx \
-    -H 'Content-Type: text/xml' \
-    -H 'SOAPAction: http://tempuri.org/Add' \
-    -d '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">
-    <soapenv:Header/>
-    <soapenv:Body>
-        <tem:Add>
-            <tem:intA>123</tem:intA>
-            <tem:intB>100</tem:intB>
-        </tem:Add>
-    </soapenv:Body>
-    </soapenv:Envelope>'
-  ERR_CODE=$?
+ERR_CODE=$?
 
-  if [[ ERR_CODE -ne 0 ]]; then
-      echo "Sleeping 20 and try again"
-      sleep 20
-  fi
-  
-done
+if [[ ERR_CODE -ne 0 ]]; then
+    echo "Exit code not zero"
+    exit 1
+fi
 
-# > response.txt
