fe44f986c78057313e9c2470f729fdf185c339c1
Chun Sing Tsui <c.s.tsui831@gmail.com>
2020-09-03 07:22:33 -0400

    updated test and task

--- dp/soap-wsp/smoke-test.sh
+++ dp/soap-wsp/smoke-test.sh
@@ -2,11 +2,11 @@
 
 # For running locally
 # export RELEASE_NAME=dp-pipe-rr
-# export TARGET_NAMESPACE=integration
+# export TARGET_NAMESPACE=dp
 
 export SOUP_ROUTE_HTTPS=$(oc get route $RELEASE_NAME-calc-https -ojsonpath='{.spec.host}' -n $TARGET_NAMESPACE)
 
-curl -k -X POST \
+curl_content=`curl -k -X POST \
 https://$SOUP_ROUTE_HTTPS/calculator.asmx \
 -H 'Content-Type: text/xml' \
 -H 'SOAPAction: http://tempuri.org/Add' \
@@ -18,12 +18,20 @@ https://$SOUP_ROUTE_HTTPS/calculator.asmx \
         <tem:intB>100</tem:intB>
     </tem:Add>
 </soapenv:Body>
-</soapenv:Envelope>'
+</soapenv:Envelope>'`
 
 ERR_CODE=$?
+
+echo "Curl Result"
+echo $curl_content
 
 if [[ ERR_CODE -ne 0 ]]; then
     echo "Exit code not zero"
     exit 1
 fi
 
+if [[ $curl_content == *"223"* ]]; then
+  echo "Sum match"
+else
+  exit 1
+fi
